7bcc3f0ad7bec8c9a623685790e8ae47f6ce2c1f
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-05-03 20:51:20 +0530

    Added : Welcome to Binary Search Tree

---
+++ src/main/java/com/bridgelabz/MyBinaryTree.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MyBinaryTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structures Binary Search Tree Program");
+    }
+}
+
